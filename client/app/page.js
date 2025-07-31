658bad4b01db67e246b4508c4bccdd3495af1281
David-Chan-Ho2 <dho12@charlotte.edu>
2025-07-31 19:31:55 -0400

    fix

--- client/app/page.js
+++ client/app/page.js
@@ -1,38 +1,35 @@
-"use strict";
 "use client";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = Home;
-const react_1 = require("react");
-const recharts_1 = require("recharts");
-function Home() {
-    const [data, setData] = (0, react_1.useState)([]);
-    (0, react_1.useEffect)(() => {
+import { useEffect, useState } from 'react';
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis, } from 'recharts';
+export default function Home() {
+    const [data, setData] = useState([]);
+    useEffect(() => {
         fetch('http://localhost:3001/api/daily-averages')
             .then((res) => res.json())
             .then(setData);
     }, []);
     return (<div className="p-4">
       <h2 className="text-xl font-semibold mb-4">Daily Weather Averages</h2>
-      <recharts_1.ResponsiveContainer width="100%" height={400}>
-        <recharts_1.LineChart data={data}>
-          <recharts_1.CartesianGrid strokeDasharray="3 3"/>
-          <recharts_1.XAxis dataKey="date"/>
-          <recharts_1.YAxis label={{ value: 'Temp (°C)', angle: -90, position: 'insideLeft' }}/>
-          <recharts_1.Tooltip />
-          <recharts_1.Legend />
-          <recharts_1.Line type="monotone" dataKey="avgTemp" stroke="#ff4c4c" name="Temperature"/>
-        </recharts_1.LineChart>
-      </recharts_1.ResponsiveContainer>
-      <recharts_1.ResponsiveContainer width="100%" height={400}>
-        <recharts_1.LineChart data={data}>
-          <recharts_1.CartesianGrid strokeDasharray="3 3"/>
-          <recharts_1.XAxis dataKey="date"/>
-          <recharts_1.YAxis orientation="left" label={{ value: 'Humidity (%)', angle: 90, position: 'insideRight' }}/>
-          <recharts_1.Tooltip />
-          <recharts_1.Legend />
-          <recharts_1.Line type="monotone" dataKey="avgHumidity" stroke="#4c6cff" name="Humidity"/>
-        </recharts_1.LineChart>
-      </recharts_1.ResponsiveContainer>
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart data={data}>
+          <CartesianGrid strokeDasharray="3 3"/>
+          <XAxis dataKey="date"/>
+          <YAxis label={{ value: 'Temp (°C)', angle: -90, position: 'insideLeft' }}/>
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="avgTemp" stroke="#ff4c4c" name="Temperature"/>
+        </LineChart>
+      </ResponsiveContainer>
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart data={data}>
+          <CartesianGrid strokeDasharray="3 3"/>
+          <XAxis dataKey="date"/>
+          <YAxis orientation="left" label={{ value: 'Humidity (%)', angle: 90, position: 'insideRight' }}/>
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="avgHumidity" stroke="#4c6cff" name="Humidity"/>
+        </LineChart>
+      </ResponsiveContainer>
     </div>);
 }
 //# sourceMappingURL=page.js.map
--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 const nextConfig = {
 /* config options here */
 };
-exports.default = nextConfig;
+export default nextConfig;
 //# sourceMappingURL=next.config.js.map
